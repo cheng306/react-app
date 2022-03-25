2b68d54530e8da5d0644069d0c2da6fd5dc7e9e3
cheng306 <johnnycheng0306@gmail.com>
2022-03-25 15:49:50 -0400

    all

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { faker } from '@faker-js/faker';
+import { faker }  from '@faker-js/faker';
 
-if (module.hot) {
-  module.hot.accept();
-}
+// if (module.hot) {
+//   module.hot.accept();
+// }
+
+
 
 const App = () => {
   return (
     <div className="ui container comments">
+      
       <div className="comment">
         <a href="/" className="avatar">
-          <img alt="avatar" src="{faker.image.avatar()}"/>
+          <img alt="avatar" src={faker.image.avatar()}/>
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00PM</span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()}/>
         </a>
         <div className="content">
           <a href="/" className="author">
